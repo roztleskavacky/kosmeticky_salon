0d6a2bfd2df80ca4f97621d18f7268f5a16edc48
Roman Pavlík <roman.pavlik@enwoxsystems.cz>
2014-11-11 19:53:55 +0100

    Inicializace aplikace

---
+++ src/main/java/com/roztleskavacky/kosmeticky_salon/HomepageController.java
@@ -0,0 +1,15 @@
+
+package com.roztleskavacky.kosmeticky_salon;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomepageController {
+
+    @RequestMapping("/")
+    public String homepage() {
+        return "frontend/index";
+    }
+    
+}
